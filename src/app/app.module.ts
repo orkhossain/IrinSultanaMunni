315a1a9b2388bf2fd07e4b9f6d958524f8864695
Ork Hossain Muntaqin <orkhoss.2011@gmail.com>
2023-12-18 18:27:30 +0100

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppBarComponent } from './app-bar/app-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MarqueeComponent } from './marquee/marquee.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppBarComponent,
+    FooterComponent,
+    MarqueeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
