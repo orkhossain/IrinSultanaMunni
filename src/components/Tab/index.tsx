3b78f575e2fee820492cc2b88fbfb52b194d8fd1
Ork Hossain Muntaqin <orkhoss.2011@gmail.com>
2023-12-23 13:21:53 +0100

    fix build

--- src/components/Tab/index.tsx
+++ src/components/Tab/index.tsx
@@ -14,64 +14,66 @@ import { getDictionary } from '@/get-dictionary'
 const AutoPlaySwipeableViewsComponent = SwipeableViews
 
 function CenteredTabs() {
-  const count = useSelector(selectCount)
-  const dispatch = useDispatch()
-  const { languages, flags } = exportData
+    const count = useSelector(selectCount)
+    const dispatch = useDispatch()
+    const { languages, flags } = exportData
 
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'))
+    const theme = useTheme()
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'))
 
-  const flagArray = Object.keys(flags)
-  const languagArray = Object.keys(languages)
-  const lan = useSelector(selectLanguage)
+    const flagArray = Object.keys(flags)
+    const languagArray = Object.keys(languages)
+    const lan = useSelector(selectLanguage)
 
-  const array = isMobile ? flagArray : languagArray
+    const array = isMobile ? flagArray : languagArray
 
-  const handleChange = (newValue: number) => {
-    dispatch(setValue(newValue))
-    dispatch(setLanguage(flags[array[newValue]] || languages[array[newValue]]))
-  }
-
-  useEffect(() => {
-    async function fetchDictionary() {
-      try {
-        const dictionary = await getDictionary(lan)
-        dispatch(setDictionary(dictionary))
-      } catch (error) {
-        console.error('Error fetching dictionary:', error)
-      }
+    const handleChange = (newValue: number) => {
+        dispatch(setValue(newValue))
+        dispatch(
+            setLanguage(flags[array[newValue]] || languages[array[newValue]])
+        )
     }
 
-    fetchDictionary()
-  }, [dispatch, lan])
+    useEffect(() => {
+        async function fetchDictionary() {
+            try {
+                const dictionary = await getDictionary(lan)
+                dispatch(setDictionary(dictionary))
+            } catch (error) {
+                console.error('Error fetching dictionary:', error)
+            }
+        }
 
-  return (
-    <>
-      <AutoPlaySwipeableViewsComponent
-        axis={'x'}
-        resistance={true}
-        index={count}
-        enableMouseEvents={true}
-        onChangeIndex={handleChange}
-        style={{
-          height: '100vh',
-          backgroundImage: `url(/countries/${count}.webp)`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          padding: '10% 2vh',
-          transition: ' background 100ms ease-in-out 300ms',
-          alignItems: 'center',
-          transformOrigin: '0 0',
-        }}
-      >
-        {array.map((language, index) => (
-          <CustomTabPanel value={count} index={index} key={index}>
-            <Content />
-          </CustomTabPanel>
-        ))}
-      </AutoPlaySwipeableViewsComponent>
-    </>
-  )
+        fetchDictionary()
+    }, [dispatch, lan])
+
+    return (
+        <>
+            <AutoPlaySwipeableViewsComponent
+                axis={'x'}
+                resistance={true}
+                index={count}
+                enableMouseEvents={true}
+                onChangeIndex={handleChange}
+                style={{
+                    height: '100vh',
+                    backgroundImage: `url(/countries/${count}.webp)`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    padding: '10% 2vh',
+                    transition: ' background 100ms ease-in-out 300ms',
+                    alignItems: 'center',
+                    transformOrigin: '0 0',
+                }}
+            >
+                {array.map((language, index) => (
+                    <CustomTabPanel value={count} index={index} key={index}>
+                        <Content />
+                    </CustomTabPanel>
+                ))}
+            </AutoPlaySwipeableViewsComponent>
+        </>
+    )
 }
 
 export default CenteredTabs
